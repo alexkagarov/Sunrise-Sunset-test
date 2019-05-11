1fda33182c3becfdde8e3cb3a4ac48173b8f7b2e
Alex Kagarov <alexkagarov@mbp-dima.rusanovka-net.kiev.ua>
2019-05-11 21:44:34 +0300

    Initial Commit

---
+++ Sunrise-Sunset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunrise-Sunset
+//
+//  Created by Alex Kagarov on 5/11/19.
+//  Copyright © 2019 Alex Kagarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
